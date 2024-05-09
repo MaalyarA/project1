1b248b38d4c6b248867ea2f8f5754d38245e3fdc
MaalyarA <167805369+MaalyarA@users.noreply.github.com>
2024-05-09 09:40:13 -0500

    Committing Main
    I had to commit main just to be able to test the program in the IDE.

--- project1_gui.py
+++ project1_gui.py
@@ -1,6 +1,13 @@
 #gui file
 from tkinter import *
 import csv
-
-
-
+from project1_functions import *
+#MAKE SURE TO ADD PRIVATE VARIABLESSSSSSS
+class GUI:
+    def __init__(self, window):
+        self.__window = window
+        self.__frame1 = Frame(self.__window)
+        self.__frame2 = Frame(self.__window)
+        self.__frame3 = Frame(self.__window)
+        self.name_label = Label(self.__frame1, text='Name')
+        self.name_entry = Entry(self.__frame1)
--- project1_main.py
+++ project1_main.py
@@ -1,3 +1,14 @@
 #main file
+from project1_functions import *
+from project1_gui import *
+def main():
+    window = Tk()
+    window.geometry("500x500")
+    window.title("ATM")
+    window.resizable(False, False)
+    GUI(window)
+    window.mainloop()
+
+
 if __name__ == '__main__':
-    pass
+    main()
